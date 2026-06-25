2f9eb800e174daf3e6eba91b3f066493288bce49
Grace Mugoiri <gracemugoiri@gmail.com>
2026-06-25 14:40:47 +0300

    refactor solution  5

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,5 @@
 # Check the total amount in the wallet.
-bitcoin-cli -regtest getbalance 
+# Create a wallet with the name "builderswallet".
+bitcoin-cli -regtest createwallet "builderswallet" | jq -r '.name'
+# Get the balance of the "builderswallet".
+bitcoin-cli -regtest -rpcwallet="builderswallet" getbalance
