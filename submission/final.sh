1b193b2ed4f28fe018895af7540938d8aa891fe1
Grace Mugoiri <gracemugoiri@gmail.com>
2026-06-25 20:11:55 +0300

    fix capstone failing for validate address for final.sh

--- submission/final.sh
+++ submission/final.sh
@@ -184,7 +184,7 @@ check_cmd "Getting address info"
 
 # STUDENT TASK: Extract the internal key (the x-only pubkey) from the descriptor
 # WRITE YOUR SOLUTION BELOW:
-INTERNAL_KEY=$(echo "$ADDR_INFO" | grep -oE '"pubkey":"[^"]+"' | cut -d '"' -f 4)
+INTERNAL_KEY=$(echo "$ADDR_INFO" | jq -r '.pubkey')
 check_cmd "Extracting key from descriptor"
 INTERNAL_KEY=$(trim "$INTERNAL_KEY")
 
